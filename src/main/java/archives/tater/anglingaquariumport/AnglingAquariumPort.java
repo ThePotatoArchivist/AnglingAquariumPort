9b865269559ebd74c9ec58b63567fddf50f4b4b4
ThePotatoArchivist <josiahfu@gmail.com>
2025-05-12 23:40:28 -0700

    Use client tags

--- src/main/java/archives/tater/anglingaquariumport/AnglingAquariumPort.java
+++ src/main/java/archives/tater/anglingaquariumport/AnglingAquariumPort.java
@@ -1,8 +1,9 @@
 package archives.tater.anglingaquariumport;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.tag.client.v1.ClientTags;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.SideShapeType;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.registry.RegistryKeys;
@@ -13,31 +14,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
 
-import java.util.List;
+import static net.fabricmc.fabric.api.tag.client.v1.ClientTags.isInWithLocalFallback;
 
-public class AnglingAquariumPort {
+public class AnglingAquariumPort implements ClientModInitializer {
     public static final String MOD_ID = "anglingaquariumport";
-
-    public static final List<Block> VANILLA_GLASS_BLOCKS = List.of(
-            Blocks.GLASS,
-            Blocks.TINTED_GLASS,
-            Blocks.WHITE_STAINED_GLASS,
-            Blocks.ORANGE_STAINED_GLASS,
-            Blocks.MAGENTA_STAINED_GLASS,
-            Blocks.LIGHT_BLUE_STAINED_GLASS,
-            Blocks.YELLOW_STAINED_GLASS,
-            Blocks.LIME_STAINED_GLASS,
-            Blocks.PINK_STAINED_GLASS,
-            Blocks.GRAY_STAINED_GLASS,
-            Blocks.LIGHT_GRAY_STAINED_GLASS,
-            Blocks.CYAN_STAINED_GLASS,
-            Blocks.PURPLE_STAINED_GLASS,
-            Blocks.BLUE_STAINED_GLASS,
-            Blocks.BROWN_STAINED_GLASS,
-            Blocks.GREEN_STAINED_GLASS,
-            Blocks.RED_STAINED_GLASS,
-            Blocks.BLACK_STAINED_GLASS
-    );
 
     public static final TagKey<Block> SIDED_GLASS_BLOCKS = TagKey.of(RegistryKeys.BLOCK, Identifier.of(MOD_ID, "sided_glass_blocks"));
 
@@ -45,6 +25,14 @@ public class AnglingAquariumPort {
         if (!fluidState.isIn(FluidTags.WATER)) return false;
         var offsetPos = pos.offset(direction);
         var state = world.getBlockState(offsetPos);
-        return AnglingAquariumPort.VANILLA_GLASS_BLOCKS.contains(state.getBlock()) || state.isIn(ConventionalBlockTags.GLASS_BLOCKS) || state.isIn(AnglingAquariumPort.SIDED_GLASS_BLOCKS) && state.isSideSolid(world, offsetPos, direction.getOpposite(), SideShapeType.FULL);
+        return isInWithLocalFallback(ConventionalBlockTags.GLASS_BLOCKS, state.getRegistryEntry()) ||
+                (isInWithLocalFallback(AnglingAquariumPort.SIDED_GLASS_BLOCKS, state.getRegistryEntry())
+                && state.isSideSolid(world, offsetPos, direction.getOpposite(), SideShapeType.FULL));
+    }
+
+    @Override
+    public void onInitializeClient() {
+        ClientTags.getOrCreateLocalTag(ConventionalBlockTags.GLASS_BLOCKS);
+        ClientTags.getOrCreateLocalTag(SIDED_GLASS_BLOCKS);
     }
 }
