57a3be3b157d22233be3a7dd9ce8de49cd8280cb
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-28 01:52:35 -0800

    26.1s10 port

--- src/main/java/archives/tater/anglingaquariumport/mixin/FluidRendererMixin.java
+++ src/main/java/archives/tater/anglingaquariumport/mixin/FluidRendererMixin.java
@@ -11,10 +11,10 @@ import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.client.renderer.block.LiquidBlockRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 
